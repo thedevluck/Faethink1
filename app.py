a827c6b316308a5eed726a3f376ad1acbd078dbb
TheDevLuck <paomijado@gmail.com>
2025-09-15 10:16:12 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -509,7 +509,7 @@ if menu == "Chatbot":
         "resposta": "Você entrega (ou apresenta, se for documento externo) junto à Secretaria Acadêmica da FAETEC / ETER República. Em geral, esse setor é responsável por registrar formalmente boletins, documentos escolares, notas e demais registros acadêmicos."
     },
     {
-        "keywords": ["solicitar", "boletim", "antigo"],
+        "keywords": ["solicito", "boletim", "antigo"],
         "resposta": "Faça uma solicitação formal à Secretaria Acadêmica, informando: seu nome completo, matrícula, curso/turma e o período do boletim que você não recebeu. Pode ser necessário preencher um requerimento (presencial ou digital) e aguardar o prazo interno para emissão."
     },
     {
